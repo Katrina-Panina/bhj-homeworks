28f45b68101690846575a11c8897c0237ea917f1
Katerina Panina <khomenko.kat@mail.ru>
2021-04-20 19:40:10 +0300

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,23 @@
+//Домашнее задание к лекции «Возможности JavaScript в браузере».Игра-кликер
+'use strict';
+
+const cookie = document.getElementById('cookie');
+const cookieClickCounter = document.getElementById('clicker__counter');
+const cookieClickSpeed = document.getElementById('clicker__speed');
+
+let clickTime = Date.now();
+
+function cookieClick() {
+    cookieClickCounter.textContent = parseInt(cookieClickCounter.textContent) + 1;
+    cookieClickSpeed.textContent = (1 / ((Date.now() - clickTime) / 1000)).toFixed(2);
+    clickTime = Date.now();
+
+    if(cookieClickCounter.textContent % 2 !== 0) {
+        this.width *= 1.3;
+        this.height *= 1.3;
+    } else {
+        this.width /= 1.3;
+        this.height /= 1.3;
+    }
+}
+cookie.onclick = cookieClick;
