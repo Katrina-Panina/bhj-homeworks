af9f91700d5ff6d6142ac356545ca05ae1e8f3e9
Katerina Panina <khomenko.kat@mail.ru>
2021-04-30 11:58:58 +0300

    first commit ads

---
+++ dom/ads/task.js
@@ -0,0 +1,25 @@
+//Домашнее задание к лекции «DOM». Ротатор рекламы
+'use strict';
+
+const firstCase = document.querySelector(".rotator").firstElementChild,
+lastCase = document.querySelector(".rotator").lastElementChild;
+
+let activeCase = document.querySelector(".rotator__case_active");
+activeCase.style.color = activeCase.dataset.color;
+
+function selectActiveCase() {
+    activeCase.classList.remove("rotator__case_active");
+
+    if (activeCase === lastCase) {
+        activeCase = firstCase;
+    } else {
+        activeCase = activeCase.nextElementSibling;
+    }
+
+    activeCase.style.color = activeCase.dataset.color;
+    activeCase.classList.add("rotator__case_active");
+    clearInterval(interval);
+    interval = setInterval(selectActiveCase, activeCase.dataset.speed);
+}
+
+let interval = setInterval(selectActiveCase, activeCase.dataset.speed);
