f979cbee18d8488d9b318af52e66ff20da61afa1
Katerina Panina <khomenko.kat@mail.ru>
2021-05-17 19:37:26 +0300

    first commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,15 @@
+//Домашнее задание к лекции «Асинхронные запросы». Загрузка больших файлов
+'use strict';
+const loading = document.getElementById('progress');
+const send = document.getElementById("send");
+
+send.addEventListener("click", (event) => {
+    const data = new FormData(document.getElementById("form"));
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+    xhr.upload.onprogress = function(event){
+        loading.value = event.loaded / event.total;
+    };
+    xhr.send(data);
+    event.preventDefault();
+});
